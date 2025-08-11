6b251122107c2ecc1c239dd0ce3d8b9429c60cb2
kjpark <kjpark@ibleaders.co.kr>
2025-08-11 09:51:48 +0900

    init commit

---
+++ src/main/java/com/abp/autoblogproj/AutoBlogProjApplication.java
@@ -0,0 +1,13 @@
+package com.abp.autoblogproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBlogProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoBlogProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abp/autoblogproj/AutoBlogProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abp.autoblogproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBlogProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
